bf66eff12e18fc892bd9de8c5a3b8109e0c7afbf
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-07 16:56:04 -0300

    Initial config

---
+++ Devote/App/Views/ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  Devote
+//
+//  Created by Ivan Romero on 07/02/2024.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+
+    @State var task: String = ""
+    
+    private var isButtonDisabled: Bool {
+        task.isEmpty
+    }
+    
+    // MARK: - FETCHING DATA
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    // MARK: - FUNCTIONS
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            newItem.task = task
+            newItem.id = UUID()
+            newItem.completion = false
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                VStack(alignment: .center, spacing: 16) {
+                    TextField("New task", text: $task)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                    Button {
+                        addItem()
+                    } label: {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                    .disabled(isButtonDisabled)
+                    .padding()
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .background(isButtonDisabled ? .gray : .pink)
+                    .clipShape(.rect(cornerRadius: 10))
+                }
+                .padding()
+                
+                List {
+                    ForEach(items) { item in
+                        VStack(alignment: .leading) {
+                            Text(item.task ?? "")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                            
+                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+            }
+            .navigationTitle("Daily Tasks")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            Text("Select an item")
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
---
+++ Devote/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by Ivan Romero on 07/02/2024.
+//
+
+import Foundation
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
