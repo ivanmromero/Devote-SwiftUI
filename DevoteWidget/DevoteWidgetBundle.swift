b8498ca27f24c8b79ddf83f9993544bedb659e5c
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-08 22:57:27 -0300

    Added static widget and setup

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Ivan Romero on 08/02/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
